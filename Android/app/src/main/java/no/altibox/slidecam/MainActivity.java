ae26a9b3b5fa291dbcae2bc05cb6dc82241a6999
Thomas Le <Thomas.le@knowit.no>
2015-04-09 21:57:54 +0200

    Added ButterKnife library to ease up our life

--- Android/app/src/main/java/no/altibox/slidecam/MainActivity.java
+++ Android/app/src/main/java/no/altibox/slidecam/MainActivity.java
@@ -3,6 +3,8 @@ package no.altibox.slidecam;
 import android.app.Activity;
 import android.os.Bundle;
 
+import butterknife.ButterKnife;
+
 
 public class MainActivity extends Activity {
 
@@ -10,6 +12,6 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        ButterKnife.inject(this);
     }
 }
